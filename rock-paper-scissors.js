01d96b3933dfccf4268a2dcea23e4f31b1e572eb
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-07 14:18:43 +0800

    Create function game, so game can be repeated
    Created function game() and placed computerSelection() and
    playerSelection() inside. Made a 5 round for loop of game() with a
    playerScore counter and computerScore counter

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,43 +1,70 @@
-// Function that returns computer's random choice
+let computerScore = 0;
+let playerScore = 0;
 
-function getComputerChoice() {
-    const computerChoices = ["rock", "paper", "scissors"]
-    let randomChoice = Math.floor(Math.random() * 3)
-    console.log(computerChoices[randomChoice])
-    return computerChoices[randomChoice]
+for (let i = 0; i < 5; i++) { 
+    game();
+    console.log(`Player:${playerScore}, Computer:${computerScore}`);
 }
 
-function playRound(player, computer) {
-    if (player === "rock") {
-        if (computer === "rock") {
-            return "It's a Draw!"
-        } else if (computer === "paper") {
-            return "You Lose! Paper beats Rock"
+function game() {
+    function getComputerChoice() {
+        const computerChoices = ["rock", "paper", "scissors"];
+        let randomChoice = Math.floor(Math.random() * 3);
+        console.log(computerChoices[randomChoice]);
+        return computerChoices[randomChoice];
+    }
+    
+    function playRound(player, computer) {
+        if (player === "rock") {
+            if (computer === "rock") {
+                result = "D";
+                return "It's a Draw!";
+            } else if (computer === "paper") {
+                result = "L";
+                return "You Lose! Paper beats Rock.";
+            } else {
+                result = "W";
+                return "You Win! Rock beats Scissors.";
+            }
+        } else if (player === "paper") {
+            if (computer === "paper") {
+                result = "D";
+                return "It's a Draw!";
+            } else if (computer === "scissors") {
+                result = "L";
+                return "You Lose! Scissors beat Paper.";
+            } else {
+                result = "W";
+                return "You Win! Paper beats Rock.";
+            }
+        } else if (player === "scissors") {
+            if (computer === "scissors") {
+                result = "D";
+                return "It's a Draw!"
+            } else if (computer === "rock") {
+                result = "L";
+                return "You Lose! Rock beats Scissors.";
+            } else {
+                result = "W";
+                return "You Win! Scissors beat Paper.";
+            }
         } else {
-            return "You Win! Rock beats Scissors"
+            return "You must have typed something wrong.";
         }
-    } else if (player === "paper") {
-        if (computer === "paper") {
-            return "It's a Draw!"
-        } else if (computer === "scissors") {
-            return "You Lose! Scissors beat Paper"
-        } else {
-            return "You Win! Paper beats Rock"
-        }
-    } else if (player === "scissors") {
-        if (computer === "scissors") {
-            return "It's a Draw!"
-        } else if (computer === "rock") {
-            return "You Lose! Rock beats Scissors"
-        } else {
-            return "You Win! Scissors beat Paper"
-        }
-    } else {
-        return "You must have typed something wrong"
+    }
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Type in your choice: Rock, Paper or Scissors?").toLowerCase();
+    let result = "";
+
+    console.log(`${playRound(playerSelection, computerSelection)}`);
+
+    if (result === "W") {
+        playerScore++;
+    } else if (result === "L") {
+        computerScore++;
+    } else if (result === "D"){
+        playerScore++;
+        computerScore++;
     }
 }
-
-let computerSelection = getComputerChoice()
-let playerSelection = prompt("Type in your choice: Rock, Paper or Scissors?").toLowerCase()
-
-console.log(playRound(playerSelection, computerSelection))
