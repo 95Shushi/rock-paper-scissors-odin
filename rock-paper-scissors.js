05fa99b62565a34bf9772b27f06650e632712075
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-07 13:11:12 +0800

    Add function that returns computer's random choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+// Function that returns computer's random choice
+
+function getComputerChoice() {
+    let computerChoices = ["Rock", "Paper", "Scissors"]
+    let randomChoice = Math.floor(Math.random() * 3)
+    console.log(computerChoices[randomChoice])
+    return computerChoices[randomChoice]
+}
